fc20f1d2cca3285c0fc2cb45df9ff6d93cc1977a
kuruv <kuruvajayashree@88>
2025-06-13 15:08:38 +0530

    loop

---
+++ src/com/datatype/Data_Type.java
@@ -0,0 +1,15 @@
+package com.datatype;
+
+public class Data_Type {
+    public static void main(String[] args){
+        boolean isCompleted = false;
+        System.out.println(isCompleted);
+        byte letter ='b';
+        System.out.println(letter);
+        char age = 13;
+        System.out.println(age);
+        float percent = 79.9f;
+       System.out.println(percent);
+
+    }
+}
