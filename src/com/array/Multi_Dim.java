a62139e14b0b44123e766cca6bed940a3ce2de7f
kuruv <kuruvajayashree@88>
2025-06-18 14:51:24 +0530

    hello

---
+++ src/com/array/Multi_Dim.java
@@ -0,0 +1,15 @@
+package com.array;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Multi_Dim {
+    public static void main(String[] args) {
+        int arr[]= {1,2,3};
+
+
+        System.out.println(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    }
