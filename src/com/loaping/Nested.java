7c64f411e132ba75b052a279044c3deabe5402e1
kuruv <kuruvajayashree@88>
2025-06-18 15:00:23 +0530

    hello

---
+++ src/com/loaping/Nested.java
@@ -0,0 +1,14 @@
+package com.loaping;
+
+public class Nested {
+    public static void main(String[] args) {
+            for (int rows = 1 ; rows<3 ; rows++){
+            for (int col = 2 ; col<3 ; col++){
+                System.out.print("column" +col+ " ");
+
+            }
+            System.out.println("rows" +rows);
+        }
+    }
+
+    }
