614760c45cd63a788d61f05162f6ca1647d5373c
kuruv <kuruvajayashree@88>
2025-06-29 07:45:36 +0530

    Addeed pattern programs

---
+++ src/com/patterns/Inverted_Tri.java
@@ -0,0 +1,13 @@
+package com.patterns;
+
+public class Inverted_Tri {
+    public static void main(String[] args) {
+    int n =5;
+    for (int i=n;i>=1;i--){
+        for (int j=1;j<=i;j++) {
+            System.out.print("*");
+        }
+        System.out.println();
+        }
+    }
+    }
---
+++ src/com/patterns/Letter_L.java
@@ -0,0 +1,19 @@
+package com.patterns;
+
+public class Letter_L {
+    public static void main(String[] args) {
+        int n = 5;
+        for(int i=1;i<=n;i++){
+            if(i<n) {
+                System.out.println("*");
+            }
+                else{
+                    for (int j=1;j<=n;j++){
+                        System.out.print("*");
+
+                }
+            }
+        }
+    }
+
+    }
